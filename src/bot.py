878e64e3f8b7e688bd4044b632be452c720ef463
William Sha <second.yukirhodes@hotmail.fr>
2018-11-27 22:51:59 +0100

    [FIX] Classification du bot
    Bug corrigé sur la classification où le résultat donnait toujours sur l'intention "salut". La saisie utilisateur est maintenant correctement découpée (https://en.wikipedia.org/wiki/Lexical_analysis#Tokenization).

--- src/bot.py
+++ src/bot.py
@@ -11,13 +11,26 @@ tweets = []
 chemin_e = 'C:\\Users\\naelg_jm9bezd\\Desktop\\tpe-bot-discord-LLN\\connaissances_bot\\entree\\'
 chemin_r = 'C:\\Users\\naelg_jm9bezd\\Desktop\\tpe-bot-discord-LLN\\connaissances_bot\\reception\\'
 
-""" Formate les jeux de données pour le classifieur """
+
 def set_training(path, intent):
-    f = open(path, 'rU')
+    """ Formate les jeux de données pour le classifieur, retourne un tableau de (phrase, sentiment).
+        Chaque ligne du fichier va être associée à l'intention correspondante.
+        Exemple d'appel: set_training('C:\\monFichierContenantDesBonjour.txt','bonjour')
+
+    Keyword arguments:
+    path -- chemin du fichier texte
+    intent -- label donné à l'intention correspondante voulue
+    """
+    f = open(path, 'r')
     samples = [(l.strip(), intent) for l in f]
     return samples
 
 def get_words_in_tweets(tweets):
+    """Récupère tous les mots d'un ensemble de (phrase, sentiment).
+    
+    Keyword arguments:
+    tweets -- tableau contenant une liste de (phrase, sentiment)
+    """
     all_words = []
     for (words, sentiment) in tweets:
       all_words.extend(words)
@@ -25,6 +38,8 @@ def get_words_in_tweets(tweets):
 
 def get_word_features(wordlist):
     wordlist = nltk.FreqDist(wordlist)
+    print('Voici les mots les plus utilisés :')
+    print(wordlist.most_common(30))
     word_features = wordlist.keys()
     return word_features
 
@@ -35,8 +50,17 @@ def extract_features(document):
         features['contains(%s)' % word] = (word in document_words)
     return features
 
+def flatten_list(multiList):
+    out = []
+    for item in multiList:
+        if isinstance(item, list):
+            out = out + flatten_list(item)
+        else:
+            out.append(item)
+    return out
+
 """ on ajoute à l'entrainement nos phrases pour que le bot s'approprie des concepts """
-# TODO : modifier les chemins des fichiers .txt (les créer aussi si nécessaire). Ce serait utile si vous vous mettiez d'accord sur un chemin commun (rappelez-vous que vous travaillez en équipe) 
+print('Alimentation de l\'entraînement du bot...')
 training += set_training(chemin_r+'blagues.txt', 'blagues')
 training += set_training(chemin_r+'ca_va.txt', 'ca va')
 training += set_training(chemin_r+'note_tpe.txt', 'note tpe')
@@ -44,15 +68,21 @@ training += set_training(chemin_r+'politique.txt', 'politique')
 training += set_training(chemin_r+'questions_prenom.txt', 'questions prenom')
 training += set_training(chemin_r+'salut.txt', 'salut')
 
+""" Filtrage de l'entrainement : on va décaler de training à tweets en ignorant les mots à une lettre,
+	qui en général ne donne pas beaucoup d'information sur le sens de la phrase.
+"""
 for (words, sentiment) in training:
-    words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
+    words_filtered = [e.lower() for e in words.split() if len(e) >= 2]
     tweets.append((words_filtered, sentiment))
 
+""" on crée notre dictionnaire à partir des mots de notre jeu de données d'entrainement """
 word_features = get_word_features(get_words_in_tweets(tweets))
-
 training_set = nltk.classify.apply_features(extract_features, tweets)
+
 """ classification et entrainement actif du bot avec les jeux de données fournis """
+print('Le bot est prêt à s\'entrainer !')
 classifier = nltk.NaiveBayesClassifier.train(training_set)
+print('Fait.')
 
 """ test de reconnaissance avec fichier test """
 user_inputs = []
@@ -63,8 +93,10 @@ if len(sys.argv) > 1:
 for user_input in user_inputs:
   valued = classifier.classify(extract_features(user_input.split()))
   limit = min(40,len(user_input))
-  print (user_input[:limit] + '\n : \t' + valued)
+  print (user_input[:limit] + '...\nIntention détectée: ' + valued)
 
 classifier.show_most_informative_features() 
+print('Précision attendue :')
+print(nltk.classify.accuracy(classifier, training_set))
 exit()
 
